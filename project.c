bd70cfcaf701d8e6fed5ccb5669a0b142020b9a5
Ojash Devkota <ojash25@gmail.com>
2020-03-09 12:07:46 +0545

    added Search and read

--- project.c
+++ project.c
@@ -66,8 +66,8 @@ void main()
 
 void Padd()
 {
-    fp = fopen("./prisoners.txt","a+"); //creates file in same directory as the program
-    fpcsv = fopen("./prisoners.csv","a+");
+    fp=fopen("C:\\Users\\ojash.OJASH\\Documents\\Projects\\C\\School Project\\stupid-project\\prisoners.txt","a+"); //creates file in same directory as the program
+    //fpcsv = fopen("./prisoners.csv","a+");
 
     printf("Enter prisoner id:");
     scanf("%d",&pri.PrisonerId);
@@ -92,10 +92,10 @@ void Padd()
     scanf("%d",&pri.release.day);
     //fflush(stdin);
 
-    fprintf(fp,"PrisonerID:%d | Prisoner Name:%s %s | Prisoner Cell no:%s | Age:%d | Crime:%s | Release Day =%d-%s-%d \n",pri.PrisonerId,pri.Prisoner_FName,pri.Prisoner_LName,pri.Prisoner_Cell,pri.age,pri.crime,pri.release.year,pri.release.month,pri.release.day);
+    fprintf(fp,"%d %s %s %s | Age:%d | Crime:%s | Release Day =%d-%s-%d \n",pri.PrisonerId,pri.Prisoner_FName,pri.Prisoner_LName,pri.Prisoner_Cell,pri.age,pri.crime,pri.release.year,pri.release.month,pri.release.day);
 
     fprintf(fpcsv,"S.N,Name,Cell,Age,Crime,Release Date\n");
-    fprintf(fpcsv,"%d,%s %s,%s,%d,%s,%d-%s-%d\n",pri.PrisonerId,pri.Prisoner_FName,pri.Prisoner_LName,pri.Prisoner_Cell,pri.age,pri.crime,pri.release.year,pri.release.month,pri.release.day);
+    fprintf(fpcsv,"%d %s %s %s %d %s %d %s %d\n",pri.PrisonerId,pri.Prisoner_FName,pri.Prisoner_LName,pri.Prisoner_Cell,pri.age,pri.crime,pri.release.year,pri.release.month,pri.release.day);
     fclose(fpcsv);
 
     fclose(fp);
@@ -109,21 +109,32 @@ void Pread()
     count = 0;
     char filename[100];
     char c;
+    char stri[100];
     system("cls");
     banner();
-    fp=fopen("./prisoner.txt","r+");
-    printf("\nEnter \n[1] to view entire data \n[2] to view data of certain prisoner\n");
-    printf(">>");
-    scanf("%d",&choice);
-    for (c = getc(fp); c != EOF; c = getc(fp)){ 
+    fp=fopen("C:\\Users\\ojash.OJASH\\Documents\\Projects\\C\\School Project\\stupid-project\\prisoners.txt","r");
+      if (fp == NULL) {
+        printf("Error! opening file");
+        // Program exits if file pointer returns NULL.
+        exit(1);
+    }
+    /*printf("\nEnter \n[1] to view entire data \n[2] to view data of certain prisoner\n");*/
+    /*for (c = getc(fp); c != EOF; c = getc(fp)){ 
         if (c == '\n'){ // Increment count if this character is newline 
             count = count + 1; 
         }
     }
-    for (int i=0; i<0;i++){
+        fscanf(fp, "%[^\n]", &stri);
+        printf("\n%s", stri);
+    for (int i=0; i<count;i++){
         fscanf(fp,"%d %s %s %s %d %s %d %s %d",&pri.PrisonerId, &pri.Prisoner_FName, &pri.Prisoner_LName,&pri.Prisoner_Cell, &pri.age, &pri.crime, &pri.release.year, &pri.release.month, &pri.release.day);
         printf("PrisonerID:%d | Prisoner Name:%s %s | Prisoner Cell no:%s | Age:%d | Crime:%s | Release Day: %d-%s-%d \n",pri.PrisonerId,pri.Prisoner_FName,pri.Prisoner_LName,pri.Prisoner_Cell,pri.age,pri.crime,pri.release.year,pri.release.month,pri.release.day);
+    }*/
+    char g;
+    while((c=fgetc(fp))!=EOF){
+        printf("%c",c);
     }
+        fclose(fp);
 }
 
 
@@ -134,6 +145,7 @@ void Psearch()
     int iden,ig;
     char c;
     int count=0;
+    fp = fopen("C:\\prisoner.txt","r");
     printf("Enter the prisoner's Id: ");
     scanf("%d",iden);
     for (c = getc(fp); c != EOF; c = getc(fp)){ 
